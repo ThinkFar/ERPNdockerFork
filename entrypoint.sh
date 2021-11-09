90cc8ca3b21fe00ab4d5093920faf9415b92856e
ThinkFar <31487797+ThinkFar@users.noreply.github.com>
2021-11-10 00:27:48 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 {
+	perl -0777 -i -pe 's/^( "socketio_port": 9000,\n "webserver_port": 8000)$/ "socketio_port": 9000/gm' /home/frappe/bench/sites/common_site_config.json
 	sudo service mysql start
 	bench start
 } || {
